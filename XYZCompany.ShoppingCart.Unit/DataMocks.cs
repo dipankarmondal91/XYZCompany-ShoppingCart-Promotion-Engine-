3619b8a30e4651cc874457c261af6aa6cf395412
Dipankar Mondal <dipankar_mondal91@yahoo.com>
2020-09-17 02:18:41 +0530

    Data mocks added

---
+++ XYZCompany.ShoppingCart.Unit/DataMocks.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XYZCompany.ShoppingCart.Promotion.Data.Models;
+using XYZCompany.ShoppingCart.Promotion.Data.types;
+
+namespace XYZCompany.ShoppingCart.Unit
+{
+    public class DataMocks
+    {
+        public Dictionary<char, QuantityPromotionRule> GetQuantityPromotionRules()
+        {
+            var rule = new Dictionary<char, QuantityPromotionRule>();
+
+            rule.Add('A', new QuantityPromotionRule()
+            {
+                PromotionAmount = 130,
+                PromotionQuatity = 3
+            });
+            rule.Add('B', new QuantityPromotionRule()
+            {
+                PromotionAmount = 45,
+                PromotionQuatity = 2
+            });
+
+            return rule;
+        }
+        public List<Sku> GetSkus()
+        {
+            var skuList = new List<Sku>();
+
+            skuList.Add(new Sku()
+            {
+                Id = 'A',
+                Price = 50
+            });
+
+            skuList.Add(new Sku()
+            {
+                Id = 'B',
+                Price = 30
+            });
+
+            skuList.Add(new Sku()
+            {
+                Id = 'C',
+                Price = 20
+            });
+
+            skuList.Add(new Sku()
+            {
+                Id = 'D',
+                Price = 15
+            });
+
+            return skuList;
+        }
+        public ComboPromotionRule GetComboPromotionRule()
+        {
+            return new ComboPromotionRule()
+            {
+                Combos = new List<char>() { 'C', 'D' },
+                PromotionAmount = 30
+            };
+        }
+        public Dictionary<char, PromotionTypes> GetSkuPromotionPairs()
+        {
+            var promotionPairs = new Dictionary<char, PromotionTypes>();
+
+            promotionPairs.Add('A', PromotionTypes.Quantity);
+            promotionPairs.Add('B', PromotionTypes.Quantity);
+            promotionPairs.Add('C', PromotionTypes.Combo);
+            promotionPairs.Add('D', PromotionTypes.Combo);
+
+            return promotionPairs;
+        }
+    }
+}
--- XYZCompany.ShoppingCart.Unit/PromotionEngineTest.cs
+++ XYZCompany.ShoppingCart.Unit/PromotionEngineTest.cs
@@ -1,23 +1,59 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using XYZ.Company.ShoppingCart.Promotion.Calculation;
+using XYZCompany.ShoppingCart.Promotion.Data.Models;
 using XYZCompany.ShoppingCart.Promotion.Data.Repository;
+using XYZCompany.ShoppingCart.Promotion.Data.types;
 
 namespace XYZCompany.ShoppingCart.Unit
 {
     public class PromotionEngineTest
     {
         private IQuery query;
+        private Mock<IQuery> mockQuery;
         private PromotionEngine promotionEngine;
+        private DataMocks dataMocks;
         [SetUp]
         public void Setup()
         {
             query = new Query();
             promotionEngine = new PromotionEngine(query);
+            mockQuery = new Mock<IQuery>();
+            dataMocks = new DataMocks();
         }
+        [Test]
+        public void CalculateTotalOrderAmount_Mock()
+        {
+            var cartItmes = new List<Cart>()
+            {
+                new Cart()
+                {
+                    Quantity = 1,
+                    SkuId = 'A'
+                },
+                new Cart()
+                {
+                    Quantity = 1,
+                    SkuId = 'B'
+                },
+                new Cart()
+                {
+                    Quantity = 1,
+                    SkuId = 'C'
+                }
+            };
+            mockQuery.Setup(x => x.GetQuantityPromotionRules()).Returns(dataMocks.GetQuantityPromotionRules());
+            mockQuery.Setup(x => x.GetSkus()).Returns(dataMocks.GetSkus());
+            mockQuery.Setup(x => x.GetComboPromotionRule()).Returns(dataMocks.GetComboPromotionRule());
+            mockQuery.Setup(x => x.GetSkuPromotionPairs()).Returns(dataMocks.GetSkuPromotionPairs());
+            var result = new PromotionEngine(mockQuery.Object).CalculateTotalOrderAmount(cartItmes);
 
+            Assert.AreEqual(100, result);
+
+        }
         [Test]
         public void CalculateTotalOrderAmount_Input1()
         {
@@ -126,5 +162,6 @@ namespace XYZCompany.ShoppingCart.Unit
             Assert.AreEqual(120, result);
 
         }
+    
     }
 }
